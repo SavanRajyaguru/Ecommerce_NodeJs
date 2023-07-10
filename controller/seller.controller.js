b879e7c3a4b2c01db75b0fa283dc0d196b845d7f
SavanRajyaguru <65443618+SavanRajyaguru@users.noreply.github.com>
2023-07-10 23:04:29 +0530

    [Feature] seller controller

---
+++ controller/seller.controller.js
@@ -0,0 +1,27 @@
+const Product = require('../models/product')
+const User = require('../models/user')
+const createHash = require('../utils/createhash.utils')
+const { statuscode, messages } = require('../utils/messages.utils')
+const { messaging } = require('../utils/messaging.utils')
+
+class SellerController{
+    async getSellerProduct(req, res){
+        try {
+            const { id } = req.decoded
+            console.log(req.decoded)
+            const isSellerExist = await User.findOne({_id: id})
+            if(!isSellerExist){
+                return messaging(res, statuscode.statusSuccess, false, 'Seller does not exist')
+            }
+            const listProduct = await Product.find({iSellerId: id}, {__v: 0})
+            if(listProduct.length == 0){
+                return messaging(res, statuscode.statusSuccess, true, 'Add some products', [])
+            }
+            return messaging(res, statuscode.statusSuccess, true, 'Product List', listProduct)
+        } catch (error) {
+            return messaging(res, statuscode.statusNotFound, false, messages.catch)
+        }
+    }
+}
+
+module.exports = new SellerController()
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,4 +1,5 @@
 const userController = require('../controller/user.controller')
+const sellerController = require('../controller/seller.controller')
 const productController = require('../controller/product.controller')
 const { isAuthorizedSeller, isAuthorizedUser } = require('../middleware/checkauthorize')
 const { authToken } = require('../middleware/checktoken')
@@ -10,6 +11,8 @@ router
     .post('/sign-up', validator.validateUserSignup, userController.signUpUser)
     .post('/login', userController.loginUser)
     .get('/get-product', authToken, isAuthorizedUser, productController.listProduct)
-    .post('/add-product', authToken, isAuthorizedSeller, validator.validateProduct, productController.addProduct), 
+    .post('/add-product', authToken, isAuthorizedSeller, validator.validateProduct, productController.addProduct) 
+    .put('/update-user', authToken, isAuthorizedUser, userController.updateUser)
+    .get('/seller-product', authToken, isAuthorizedSeller, sellerController.getSellerProduct)
 
 module.exports = router
